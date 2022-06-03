adb96f7979d76439fc26eaec12f6b0292ff89724
Kiefer Weis <kieferweis@protonmail.com>
2022-06-02 20:59:45 -0400

    Working rapper API

---
+++ server.js
@@ -0,0 +1,62 @@
+// Dependencies & variables
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+// Object to serve
+const rappers = {
+  '21 savage': {
+    'birthName': 'Sheyaa Bin Abraham-Joseph',
+    'birthLocation': 'London, England',
+    'age': 29,
+  },
+  'chance the rapper': {
+    'birthName': 'Chancelor Johnathan Bennett',
+    'birthLocation': 'Chicago, Illinois, USA',
+    'age': 29,
+  },
+  'tyler the creator': {
+    'birthName': 'Tyler Gregory Okonma',
+    'birthLocation': 'Hawthorne, California, USA',
+    'age': 31,
+  },
+  'mf doom': {
+    'birthName': 'Daniel Dumile',
+    'birthLocation': 'London, England',
+    'age': 49,
+  },
+  'dylan': {
+    'birthName': 'Dylan',
+    'birthLocation': 'Dylan',
+    'age': 'Dylan',
+  },
+  'unknown': {
+    'birthName': 'unknown',
+    'birthLocation': 'unknown',
+    'age': 0,
+  }
+}
+
+
+// Serve up a file (index.html) when they request the main page
+app.get('/', (request, response)=>{
+  response.sendFile(__dirname + '/index.html');
+})
+
+
+// Serve up the JSON for their information request (api)
+app.get('/api/:name', (request, response)=>{
+  const rapperName = request.params.name.toLowerCase();
+  if (rappers[rapperName]) {
+    response.json(rappers[rapperName]);
+  } else {
+    response.json(rappers['unknown']);
+  }
+  
+})
+
+
+// Set up server to listen on the PORT
+app.listen(PORT, _=>{
+  console.log(`The server is running on port ${PORT}. Betta go catch it!`);
+})
